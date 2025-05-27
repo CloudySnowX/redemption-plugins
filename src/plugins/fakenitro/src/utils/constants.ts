ce7fca8b6a877cb100f96f9e28f0dcea7e9f7a86
CloudySnow <cloudysn0w.x@gmail.com>
2025-05-27 08:25:46 +0200

    Create constants.ts

---
+++ src/plugins/fakenitro/src/utils/constants.ts
@@ -0,0 +1,8 @@
+export const EMOJI_SIZES = [32, 48, 56, 64, 96, 128];
+
+export const EMOJI_QUALITIES = [
+  { label: "WebP (Smaller)", value: "webp" },
+  { label: "Lossless (Better Quality)", value: "lossless" }
+];
+
+export const DEFAULT_CUSTOM_TEXT = "🔗";
