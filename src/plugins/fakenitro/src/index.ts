ec54a5bd7242b722b339ee627071db5e62494c96
CloudySnow <cloudysn0w.x@gmail.com>
2025-05-27 08:29:07 +0200

    Create index.ts

---
+++ src/plugins/fakenitro/src/index.ts
@@ -0,0 +1,31 @@
+import { registerSettings } from "@vendetta/settings";
+import Settings from "./components/Settings";
+import { patches } from "./patches";
+
+let settingsUnregister: () => void;
+
+export default {
+  onLoad: () => {
+    // Register settings
+    settingsUnregister = registerSettings("FakeNitro", Settings);
+    
+    // Apply patches
+    patches.forEach(patch => {
+      const unpatch = patch.apply();
+      if (typeof unpatch === "function") {
+        patch.remove = unpatch;
+      }
+    });
+  },
+  onUnload: () => {
+    // Unregister settings
+    settingsUnregister?.();
+    
+    // Remove patches
+    patches.forEach(patch => {
+      if (typeof patch.remove === "function") {
+        patch.remove();
+      }
+    });
+  }
+};
